a1f4fa0d757d1c2eb4dd1be52ee7592212c72e58
mpanicker <m_panicker@yahoo.com>
2018-11-05 07:27:29 -0500

    ssss

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -25,7 +25,7 @@ include_recipe "apache2"
 include_recipe "mysql::client"
 include_recipe "mysql::server"
 include_recipe "php"
-include_recipe "php::module_mysql"
+include_recipe "mysql"
 include_recipe "apache2::mod_php5"
 include_recipe "mysql::ruby"
 
